ce90bf1cbe0e467a70e467523cb3ce0ccc6ef3d2
talhakhan7640 <talhakhan7640@gmail.com>
2023-03-04 19:11:50 +0530

    changed AuthRoutes to AuthRoute

---
+++ src/components/AuthRoute.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Cookies from "universal-cookie";
+
+const AuthRoute = ({component}) => {
+  const cookie = new Cookies();
+  console.log(cookie.get("TOKEN"));
+  if (cookie.get("TOKEN") == undefined) {
+    return (
+      <div>
+        <span>You have to be login first</span>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        {component}
+      </div>
+    );
+  }
+}
+
+
+export default AuthRoute;
