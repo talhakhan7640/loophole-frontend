7e10bf819b5b88773b20954776ee4b8690f51a4e
talhakhan7640 <talhakhan7640@gmail.com>
2023-03-04 19:02:07 +0530

    Added AuthRoute to authorize the user before accessing the component

--- src/index.js
+++ src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import Registration from './components/Registration';
-import Login from './components/Login';
+import Registration from "./pages/Registration";
+import Login from "./pages/Login";
+import AuthRoute from "./components/AuthRoute";
+import UserProfile from "./pages/UserProfile";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/loophole/users/signup" />} />
+
         <Route path="loophole">
+          <Route path="home/" element={<AuthRoute component={<App />} />} />
+
+          <Route
+            path="user-profile/"
+            element={<AuthRoute component={<UserProfile />} />}
+          />
+
           <Route path="users/">
             <Route path="signup" element={<Registration />} />
             <Route path="login" element={<Login />} />
@@ -23,5 +33,3 @@ root.render(
     </BrowserRouter>
   </React.StrictMode>
 );
-
-
