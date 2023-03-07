be9420ea8d0e07bee4418e797b8625526782102a
talhakhan7640 <talhakhan7640@gmail.com>
2023-03-07 12:57:13 +0530

    made layout for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import Cookies from 'universal-cookie';
+import React from "react";
+import Cookies from "universal-cookie";
+import Search from "../components/Search";
 
-const Home = () => {
+const Home = ({username}) => {
+  console.log(username)
   const cookie = new Cookies();
-  console.log(cookie.get("TOKEN"))
+  console.log(cookie.get("TOKEN"));
   const logoutUser = (e) => {
     e.preventDefault();
-    cookie.remove("TOKEN", {path: "/"});
+    cookie.remove("TOKEN", { path: "/" });
     console.log(cookie.get("TOKEN"));
     setInterval(() => {
-    window.location.assign("users/login");
-    }, 3000)
-    
-  }
-  return(
-<div>
-    <p>This is home page</p>
-    <button onClick={logoutUser}>Logout</button>
-</div>
-  ) 
-}
+      window.location.assign("users/login");
+    }, 3000);
+  };
+  return (
+    <div className="loophole-home">
+      <div className="navbar grid grid-cols-3 gap-4">
+        <div className="col-span-1">col 1</div>
+        <div className="col-span-1">
+          <Search />
+        </div>
+        <div className="col-span-1">
+          <span>{username}</span>
+          <button onClick={logoutUser}>Logout</button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
