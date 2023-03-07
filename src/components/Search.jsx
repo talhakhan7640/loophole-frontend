139659642e674ed468d9154926e19192efa08af9
talhakhan7640 <talhakhan7640@gmail.com>
2023-03-07 12:58:33 +0530

    added search component in application

---
+++ src/components/Search.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = () => {
+    return (
+        <div>
+            <input type="search" name="" id="" />
+        </div>
+    )
+}
+
+export default Search;
